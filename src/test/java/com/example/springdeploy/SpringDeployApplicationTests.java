3c692e05761a2b3b4a08bb26267653566e7ba74a
[Johan] <johancfuentes03@gmail.com>
2023-03-18 18:22:27 -0500

    spring heroku app

---
+++ src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
